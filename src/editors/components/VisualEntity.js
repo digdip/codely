8c1eddb39784e288dd21cebb807dfaa654fe2b16
ziso <ziso@hpe.com>
2017-02-06 15:28:18 +0200

    use css transition to animate the changes in the visual editor

--- src/editors/components/VisualEntity.js
+++ src/editors/components/VisualEntity.js
@@ -6,11 +6,11 @@ class VisualizingEntity extends Component {
     render () {
         let properties = this.props.data.toJS()
         return (
-            <svg width={properties.width * appConstants.GRID_SIZE_PIXELS}
+            <svg className='animate' width={properties.width * appConstants.GRID_SIZE_PIXELS}
                  height={properties.height * appConstants.GRID_SIZE_PIXELS}
                  style={{position: "absolute", top: properties.upDown * appConstants.GRID_SIZE_PIXELS + 'px', left : properties.leftRight * appConstants.GRID_SIZE_PIXELS + 'px'}}>
 
-                    <rect width={properties.width * appConstants.GRID_SIZE_PIXELS}
+                    <rect className='animate' width={properties.width * appConstants.GRID_SIZE_PIXELS}
                           height={properties.height * appConstants.GRID_SIZE_PIXELS}
                           style={{ fill: 'RED', strokeWidth: "2", stroke: "rgb(0,0,0)"}} />
             </svg>
