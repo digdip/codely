65c246fa1ec4d145e5612132f6f20992add23dc3
ziso <ziso@hpe.com>
2017-02-19 19:54:22 +0200

    try to fix heroku build problem

--- config/project.config.js
+++ config/project.config.js
@@ -27,6 +27,7 @@ const config = {
   server_host : ip.address(), // use string 'localhost' to prevent exposure on local network
   server_port : process.env.PORT || 3000,
 
+  
   // ----------------------------------
   // Compiler Configuration
   // ----------------------------------
